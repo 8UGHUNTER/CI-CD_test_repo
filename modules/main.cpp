ba6e40d284035279227e408f0131c26694a8c8bc
Sirjeleshka <sirjeleshka@gmail.com>
2021-11-02 15:37:33 +0200

    Added file for testing module

---
+++ modules/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include "calc_module.h"
+
+
+using namespace std;
+
+int main()
+{
+	float input[5] = {0, -1, 5, 1.5, 3.14};
+	float output[5] = {0, -3.14, 15.7, 4.71, 9.8596};
+	ofstream result;
+	result.open("Result_of_testing");
+	for(int i = 0; i < 5; i++)
+	{
+		result << "Test " << i+1;
+		if(output[i] == pi_count(input[i]))
+			result << " true\n";
+		else
+			result << " false\n";
+	}
+	result.close();
+	return 0;
+}
